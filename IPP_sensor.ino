7b58f0a49a3fd9003bd156c28cad5f7edb58742e
nathaliebahadori <natha.baha@gmail.com>
2025-04-21 00:20:00 +0200

    Add files via upload

--- IPP_sensor.ino
+++ IPP_sensor.ino
@@ -1,15 +1,37 @@
 int sensorPin =; //LDR till en analog pin 
-int sensorValue = 0; //lagrar jusvärde
+int sensorValue = 0; // lagrar jusvärde
 int threshold=; // tröskelvärde för "mörker"
+
+int colorBalance = 100; //0-255, varmt till kallt ljus
+bool autoMode = true // automatisk ljusstyrning
 
 void setup() 
 {
   // put your setup code here, to run once:
   pinMode (ledPin, OUTPUT);
+  Serial.begin(9600); //Bluetooth använder samma port
 }
 
 void loop() 
 {
+  if (Serial.available())
+    {
+      char c = Serial.read();
+
+
+    }
+
   // put your main code here, to run repeatedly:
 
+
+
+  // Skicka status till appen
+  Serial.print("Ljus: ");
+  Serial.print(lightValue);
+  Serial.print(", ljusstyrka: ");
+  Serial.print(brightness);
+  Serial.print(", temp: ");
+  Serial.pointln(colorBalance);
+
+  delay(200);
 }
